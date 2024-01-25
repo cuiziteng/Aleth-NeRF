30af68bed2ee83b29de6835b97bfd3f901390d01
Cui Ziteng <55396225+cuiziteng@users.noreply.github.com>
2024-01-25 12:25:25 +0800

    Update run_LOM_aleth.sh

--- run/run_LOM_aleth.sh
+++ run/run_LOM_aleth.sh
@@ -1,10 +1,10 @@
 # buu
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 run.py --ginc configs/LOM/aleth_nerf/aleth_nerf_buu.gin --logbase ./logs 
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 run.py --ginc configs/LOM/aleth_nerf/aleth_nerf_buu.gin --logbase ./logs --con 12 --eta 0.45 
 #chair
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 run.py --ginc configs/LOM/aleth_nerf/aleth_nerf_chair.gin --logbase ./logs 
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 run.py --ginc configs/LOM/aleth_nerf/aleth_nerf_chair.gin --logbase ./logs --con 12 --eta 0.45 
 # sofa
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 run.py --ginc configs/LOM/aleth_nerf/aleth_nerf_sofa.gin --logbase ./logs 
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 run.py --ginc configs/LOM/aleth_nerf/aleth_nerf_sofa.gin --logbase ./logs --con 12 --eta 0.45 
 # bike
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 run.py --ginc configs/LOM/aleth_nerf/aleth_nerf_bike.gin --logbase ./logs 
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 run.py --ginc configs/LOM/aleth_nerf/aleth_nerf_bike.gin --logbase ./logs --con 12 --eta 0.45 
 # shrub
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 run.py --ginc configs/LOM/aleth_nerf/aleth_nerf_shrub.gin --logbase ./logs 
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 run.py --ginc configs/LOM/aleth_nerf/aleth_nerf_shrub.gin --logbase ./logs --con 12 --eta 0.45 
